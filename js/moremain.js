6f3bdb053b6ed83f67587614d8aebb092923ab83
Jason Hogrefe <hogrefejason@gmail.com>
2015-04-16 16:12:13 -0500

    Trying to fix _main.js not loading by renaming the file.

--- js/moremain.js
+++ js/moremain.js
@@ -1,16 +1,6 @@
 /*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
 (function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
 
- /**
- * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
- * Want to see more similar demos and tutorials?
- * Help by spreading the word about Ihatetomatoes blog.
- * Facebook - https://www.facebook.com/ihatetomatoesblog
- * Twitter - https://twitter.com/ihatetomatoes
- * Google+ - https://plus.google.com/u/0/109859280204979591787/about
- * Article URL: http://ihatetomatoes.net/how-to-create-a-parallax-scrolling-website-part-2/
- */
-
 ( function( $ ) {
 	
 	// Setup variables
